583205e14d3ca45595f78d0a622aae9fd3cc748e
Beni-niyogisubizo <b.niyogisub@alustudent.com>
2024-11-08 00:19:35 +0200

    Tasks 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+"""An empty class"""
 
 
 class BaseGeometry:
-    """this class represents a base geometry"""
-
-    def area(self):
-        """method not implemented yet"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates a value as an integer
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+    """
+    An empty class for geometry-related operations.
+    """
+    pass
