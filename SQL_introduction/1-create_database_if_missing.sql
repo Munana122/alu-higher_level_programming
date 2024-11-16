51295e8aeb282b860730c278a6af7bb53e378dca
Munana122 <m.munana@alustudent.com>
2024-11-16 02:25:39 -0800

    correcting errors again

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Variables for MySQL connection
-USER="your_username"
-PASSWORD="your_password"
+# This script creates the database 'hbtn_0c_0' in MySQL.
+# If the database already exists, it will not fail.
+# SQL keywords are in uppercase as per SQL conventions.
+# This script does not use SELECT or SHOW statements.
 
-# Create the database
+# Variables for MySQL connection
+USER="your_username"       # MySQL username
+PASSWORD="your_password"   # MySQL password
+
+# Create the database 'hbtn_0c_0' if it does not already exist
 mysql -u "$USER" -p"$PASSWORD" -e "CREATE DATABASE IF NOT EXISTS hbtn_0c_0;"
+
+# Output message indicating the status of the database creation
+echo "Database 'hbtn_0c_0' has been created (if it didn't exist already)."
+
 
