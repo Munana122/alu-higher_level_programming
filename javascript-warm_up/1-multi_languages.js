adeba5fab95614260576748efc99b744638f9492
Munana122 <m.munana@alustudent.com>
2025-01-31 02:53:14 -0800

    correcting errors

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log('c is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
