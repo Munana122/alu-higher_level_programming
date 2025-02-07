694d0edd04efbb2f3e961f20ced632d519df6e46
Munana122 <m.munana@alustudent.com>
2025-02-07 13:24:49 -0800

    commit

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
+// Script to read and print the content of a file
 const fs = require('fs');
+const filePath = process.argv[2]; // First argument as file path
 
-const filepath = process.argv[2];
+if (!filePath) {
+  console.error('Please provide a file path as an argument.');
+  process.exit(1);
+}
 
-fs.readFile(filepath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
+    return;
   }
+  console.log(data);
 });
+
